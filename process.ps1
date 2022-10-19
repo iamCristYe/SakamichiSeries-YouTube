cd337d96ca98afae59b0ea93112ef6f4de296659
iamcristye@outlook.com <iamcristye@outlook.com>
2022-10-19 10:34:37 +0800

    日向坂46 8thシングル「月と星が踊るMidnight」

--- process.ps1
+++ process.ps1
@@ -3,7 +3,7 @@
 # N
 yt-dlp --playlist-start 262 -f 140 -o '%(playlist)s/%(playlist_index)03d - %(title)s - %(id)s.%(ext)s' "PLslx0ZK4M9zfRUTXxZfbOgnTp4MuLcTjf"
 # H
-yt-dlp --playlist-start  92 -f 140 -o '%(playlist)s/%(playlist_index)03d - %(title)s - %(id)s.%(ext)s' "PLslx0ZK4M9zertJb1q9I4vL3Z8IMtIw4D"
+yt-dlp --playlist-start  99 -f 140 -o '%(playlist)s/%(playlist_index)03d - %(title)s - %(id)s.%(ext)s' "PLslx0ZK4M9zertJb1q9I4vL3Z8IMtIw4D"
 # S
 yt-dlp --playlist-start  35 -f 140 -o '%(playlist)s/%(playlist_index)03d - %(title)s - %(id)s.%(ext)s' "PLslx0ZK4M9ze0MqtIiTguDfmP0eOvdj2i"
 
