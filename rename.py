5ec21b6a3fc28d518fd3851211304f64508b4b63
C <iamCristYe@Gmail.com>
2023-03-07 20:19:01 -0800

    櫻坂46 5thシングル「桜月」

--- rename.py
+++ rename.py
@@ -3,7 +3,7 @@
 # H
 # yt-dlp --playlist-start  99 -f 140 -o '%(playlist)s/%(playlist_index)03d - %(title)s - %(id)s.%(ext)s' "PLslx0ZK4M9zertJb1q9I4vL3Z8IMtIw4D"
 # S
-# yt-dlp --playlist-start  35 -f 140 -o '%(playlist)s/%(playlist_index)03d - %(title)s - %(id)s.%(ext)s' "PLslx0ZK4M9ze0MqtIiTguDfmP0eOvdj2i"
+# yt-dlp --playlist-start  42 -f 140 -o '%(playlist)s/%(playlist_index)03d - %(title)s - %(id)s.%(ext)s' "PLslx0ZK4M9ze0MqtIiTguDfmP0eOvdj2i"
 
 # python3 rename.py
 
