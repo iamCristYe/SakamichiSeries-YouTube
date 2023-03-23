9d2e5b74546ac16295c9393e08bd1a8f9e2fc6fe
C <iamCristYe@Gmail.com>
2023-03-23 19:48:05 +0800

    乃木坂46 32ndシングル「人は夢を二度見る」

--- rename.py
+++ rename.py
@@ -1,5 +1,5 @@
 # N
-# yt-dlp --playlist-start 270 -f 140 -o '%(playlist)s/%(playlist_index)03d - %(title)s - %(id)s.%(ext)s' "PLslx0ZK4M9zfRUTXxZfbOgnTp4MuLcTjf"
+# yt-dlp --playlist-start 276 -f 140 -o '%(playlist)s/%(playlist_index)03d - %(title)s - %(id)s.%(ext)s' "PLslx0ZK4M9zfRUTXxZfbOgnTp4MuLcTjf"
 # H
 # yt-dlp --playlist-start  99 -f 140 -o '%(playlist)s/%(playlist_index)03d - %(title)s - %(id)s.%(ext)s' "PLslx0ZK4M9zertJb1q9I4vL3Z8IMtIw4D"
 # S
