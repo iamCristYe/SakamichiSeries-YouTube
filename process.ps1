75decdee52e93191efdfbf8db1121cf68c39674e
C <iamCristYe@Gmail.com>
2023-04-12 08:36:12 +0800

    init

---
+++ process.ps1
@@ -0,0 +1,15 @@
+bash -c "python3 rename.py reverse"
+
+# N
+$N_count = (Get-ChildItem "乃木坂46全曲 All Songs from Nogizaka46" | Measure-Object).count
+yt-dlp --playlist-start $N_count -f 140 -o '%(playlist)s/%(playlist_index)03d - %(title)s - %(id)s.%(ext)s' "PLslx0ZK4M9zfRUTXxZfbOgnTp4MuLcTjf"
+# H
+$H_count = (Get-ChildItem "日向坂46全曲 All Songs from Hinatazaka46" | Measure-Object).count
+yt-dlp --playlist-start $H_count -f 140 -o '%(playlist)s/%(playlist_index)03d - %(title)s - %(id)s.%(ext)s' "PLslx0ZK4M9zertJb1q9I4vL3Z8IMtIw4D"
+# S
+$S_count = (Get-ChildItem "櫻坂46全曲 All Songs from Sakurazaka46" | Measure-Object).count
+yt-dlp --playlist-start $S_count -f 140 -o '%(playlist)s/%(playlist_index)03d - %(title)s - %(id)s.%(ext)s' "PLslx0ZK4M9ze0MqtIiTguDfmP0eOvdj2i"
+
+bash -c "python3 rename.py rename"
+
+bash -c "sha256sum */* > sha256sum"
--- rename.py
+++ rename.py
@@ -1,12 +1,6 @@
 import os
 import re
 
-os.makedirs("SakamichiSeries", exist_ok=True)
-os.makedirs("乃木坂46全曲 All Songs from Nogizaka46", exist_ok=True)
-os.makedirs("日向坂46全曲 All Songs from Hinatazaka46", exist_ok=True)
-os.makedirs("櫻坂46全曲 All Songs from Sakurazaka46", exist_ok=True)
-os.makedirs("欅坂46全曲 All Songs from Keyakizaka46", exist_ok=True)
-
 
 def reverse(dir_name):
     group_letter = dir_name.split()[-1][0]
@@ -20,12 +14,6 @@ def reverse(dir_name):
                 os.rename(f"SakamichiSeries/{filename}", f"{dir_name}/{previous_name}")
             except OSError as err:
                 print("OS Error: {0}{1} already exists.".format(err, previous_name))
-
-
-reverse("乃木坂46全曲 All Songs from Nogizaka46")
-reverse("日向坂46全曲 All Songs from Hinatazaka46")
-reverse("櫻坂46全曲 All Songs from Sakurazaka46")
-reverse("欅坂46全曲 All Songs from Keyakizaka46")
 
 
 def rename(dir_name):
@@ -60,7 +48,22 @@ def rename(dir_name):
                 print("OS Error: {0}{1} already exists.".format(err, new_name))
 
 
-rename("乃木坂46全曲 All Songs from Nogizaka46")
-rename("日向坂46全曲 All Songs from Hinatazaka46")
-rename("櫻坂46全曲 All Songs from Sakurazaka46")
-rename("欅坂46全曲 All Songs from Keyakizaka46")
+os.makedirs("SakamichiSeries", exist_ok=True)
+os.makedirs("乃木坂46全曲 All Songs from Nogizaka46", exist_ok=True)
+os.makedirs("日向坂46全曲 All Songs from Hinatazaka46", exist_ok=True)
+os.makedirs("櫻坂46全曲 All Songs from Sakurazaka46", exist_ok=True)
+os.makedirs("欅坂46全曲 All Songs from Keyakizaka46", exist_ok=True)
+
+import sys
+
+mode = sys.argv[1]
+if mode == "reverse":
+    reverse("乃木坂46全曲 All Songs from Nogizaka46")
+    reverse("日向坂46全曲 All Songs from Hinatazaka46")
+    reverse("櫻坂46全曲 All Songs from Sakurazaka46")
+    reverse("欅坂46全曲 All Songs from Keyakizaka46")
+if mode == "rename":
+    rename("乃木坂46全曲 All Songs from Nogizaka46")
+    rename("日向坂46全曲 All Songs from Hinatazaka46")
+    rename("櫻坂46全曲 All Songs from Sakurazaka46")
+    rename("欅坂46全曲 All Songs from Keyakizaka46")
